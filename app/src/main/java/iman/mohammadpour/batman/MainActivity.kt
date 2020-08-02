2a35f8873bb75ab2723a10a089866e761857db0c
imany <imany.apk@gmail.com>
2020-08-02 10:27:16 +0430

    Initial commit

---
+++ app/src/main/java/iman/mohammadpour/batman/MainActivity.kt
@@ -0,0 +1,13 @@
+package iman.mohammadpour.batman
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
